2384d8af33dbdc6526610de554206c38e7c87f1f
Sviatoslava13 <sviatoslavamar@gmail.com>
2022-09-28 01:10:44 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,27 +1,24 @@
 const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
-
 startBtn.addEventListener('click', activeStartBtn);
 stopBtn.addEventListener('click', inActiveBtn);
 
 function activeStartBtn() {
-  timerIdi = setInterval(() => {
+  timerId = setInterval(() => {
     document.body.style.background = getRandomHexColor();
   }, 1000);
   if (startBtn) {
     startBtn.disabled = true;
   }
-
   stopBtn.disabled = false;
 }
 function inActiveBtn() {
-  clearInterval(timerIdi);
+  clearInterval(timerId);
   startBtn.disabled = false;
   if (stopBtn) {
     stopBtn.disabled = true;
   }
 }
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
