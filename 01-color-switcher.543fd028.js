be367aadb272855925189efe7cbbcf4cc11e8fdd
Sviatoslava13 <109517186+Sviatoslava13@users.noreply.github.com>
2022-09-27 22:17:02 +0000

    Deploying to gh-pages from @ Sviatoslava13/goit-js-hw-09@67ff7d6ea9fb1a727f5ab14c8b8fe0f3b2025304 🚀

---
+++ 01-color-switcher.543fd028.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");e.addEventListener("click",(function(){d=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e&&(e.disabled=!0);t.disabled=!1})),t.addEventListener("click",(function(){clearInterval(d),e.disabled=!1,t&&(t.disabled=!0)}));let d=null;
+//# sourceMappingURL=01-color-switcher.543fd028.js.map
